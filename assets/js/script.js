439e7405f9a8e4377a95cdc0c44e1acedc57732a
Alena18 <sun180983@gmail.com>
2023-01-05 15:28:02 +0000

    Add javascript for timer and flips buttons

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,43 +1,45 @@
+/* jshint esversion: 11 */
+
 var cards = document.querySelectorAll('.card');
 
 let hasFlipped = false;
 let cardOne, cardTwo;
-let lockedCard = false ; // can't flip another card until previous cards action completed
+let lockedCard = false; // can't flip another card until previous cards action completed
 
 // Find the same card and leave it flipped
 const flippedCard = e => {
-        if (lockedCard) return; 
+  if (lockedCard) return;
 
-         const target = e.target.parentElement;
+  const target = e.target.parentElement;
 
-         if (target === cardOne) return; // if you click the same card twice it stays open, as 
-         // it reacts as the same class while this card should flip back 
+  if (target === cardOne) return; // if you click the same card twice it stays open, as 
+  // it reacts as the same class while this card should flip back 
 
-         target.classList.add("flipped");
-        
-         // Get cards by one tag
-         console.log(target.dataset.animal);
-         if (!hasFlipped) {
-          // First click
+  target.classList.add("flipped");
 
-          hasFlipped = true;
-          cardOne = target;
-         } else {
-          // Second click
+  // Get cards by one tag
+  console.log(target.dataset.animal);
+  if (!hasFlipped) {
+    // First click
 
-          hasFlipped = false;
-          cardTwo = target;
-          checkMatch();
-        }
-              
-        
+    hasFlipped = true;
+    cardOne = target;
+  } else {
+    // Second click
+    addFlipCount();
+    hasFlipped = false;
+    cardTwo = target;
+    checkMatch();
+  }
+
+
 };
 
 const checkMatch = () => {
-            const isEqual = cardOne.dataset.animal === cardTwo.dataset.animal;
-            isEqual ? disCards() : unflip();
+  const isEqual = cardOne.dataset.animal === cardTwo.dataset.animal;
+  isEqual ? disCards() : unflip();
 
-  };
+};
 
 // disable cards function
 const disCards = () => {
@@ -47,63 +49,77 @@ const disCards = () => {
 
 // unflip cards function
 const unflip = () => {
-        lockedCard = true; 
+  lockedCard = true;
 
-        setTimeout(() =>{
-              cardOne.classList.remove("flipped");
-              cardTwo.classList.remove("flipped");
-              resetBoard();
-            }, 1000)
+  setTimeout(() => {
+    cardOne.classList.remove("flipped");
+    cardTwo.classList.remove("flipped");
+    resetBoard();
+  }, 1000)
 };
 
 const resetBoard = () => {
-  
+
   hasFlipped = lockedCard = false;
   cardOne = cardTwo = null;
-  
+
 }
 
 // Add Event Listener to every card
-  cards.forEach(card => {
+cards.forEach(card => {
   card.addEventListener('click', flippedCard);
-//shuffle cards for next game
+  //shuffle cards for next game
   const randomShuffle = Math.floor(Math.random() * cards.length);
   card.style.order = randomShuffle;
 });
 // Time
 // timeTag = document.querySelector(".timer"),
 // flipsTag = document.querySelector(".flips"),
-const  startTime = 0.1;
+const startTime = 0.1;
 let time = startTime * 60;
 const timeTag = document.getElementById('timer');
-
-function startTimer = () => {
-    if(hasFlipped){
-      hasFlipped = true;
-      setInterval(countTimer, 1000)}
-  };
-    
-function countTimer() {
-  
-  let minutes = Math.floor(time / 60);
-  let seconds = time % 60;
-  
-  minutes = minutes < 1 ? "0" + minutes : minutes;
-  seconds = seconds < 10 ? "0" + seconds : seconds;
-  
-  
-
-  timeTag.innerHTML = `${minutes}: ${seconds}`;
-  if (time < 0){
-         ;
-                             };
-  time--;
+let timerInterval;
+startTimer();
+function startTimer() {
+  timerInterval = setInterval(countTimer, 1000)
 };
 
+function countTimer() {
+
+  let minutes = Math.floor(time / 60);
+  let seconds = time % 60;
+
+  minutes = minutes < 10 ? "0" + minutes : minutes;
+  seconds = seconds < 10 ? "0" + seconds : seconds;
+
+
+
+  timeTag.innerHTML = `${minutes}: ${seconds}`;
+  time--;
+  if (time < 0) {
+    clearInterval(timerInterval);
+    timeOver();
+  };
+
+};
 
 // Restart game
-document.querySelector('.restart').addEventListener('click', function(){
-  window.location.reload();  
+document.querySelector('.restart').addEventListener('click', function () {
+  window.location.reload();
   return false;
-  
+
 });
+
+function timeOver() {
+  cards.forEach(card => {
+        card.classList.add("disable");
+  });
+}
+
+let flipCount = 0;
+let flipCounter = document.getElementById("flips");
+
+function addFlipCount(){
+  flipCount +=1;
+  flipCounter.innerText = flipCount;
+};
